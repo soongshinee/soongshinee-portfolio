48edadf5d1c4204c3ae92477d3a928112d60eb55
soongshinee <soongshinee@gmail.com>
2026-03-24 20:57:10 +0800

    added project 1 object

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,5 +1,17 @@
 import React from 'react'
 
+const projects = [
+  {
+    title: "Weather Dashboard Web App",
+    description: "A responsive weather dashboard application that fetches and displays real-time and forecast weather data using external APIs. Users can search for cities to view current conditions, temperature, humidity, and multi-day forecasts. The application is designed with a clean, modular UI and handles loading states, error scenarios, and dynamic data rendering.",
+    image: "Pro Cahaya Industries Sdn Bhd",
+    tags:
+      "React.js JavaScript (ES6+) REST API Integration Tailwind CSS Responsive Design State Management (useState, useEffect)",
+    link: "#",
+    github: "#"
+  }
+];
+
 const Projects = () => {
   return (
     <section>
